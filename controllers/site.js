7b3a7f5b9d48d774a3d93721a4ad3e7dc5e9ee8e
liyifei <flylee.bjfu@gmail.com>
2012-09-21 18:19:53 +0800

    controller

--- controllers/site.js
+++ controllers/site.js
@@ -1,4 +1,42 @@
+
 exports.home = function (req, res, next) {
 	//res.send("hahah");
 	res.render('index.html');
+};
+
+exports.combine = function (req, res, next) {
+	
+
+	var exec = require('child_process').exec, child;
+	var spawn = require('child_process').spawn,
+    	ping  = spawn('ping', ['www.baidu.com']);
+
+    var resData = "";
+	//req.setEncoding("utf8");
+    
+    ping.stdout.on('data', function (data) {
+    	resData += data;
+    	console.log(data);
+  		//res.send(resData);
+	});
+	ping.stderr.on('data', function (data) {
+	    console.log('stderr: ' + data);
+	});
+
+	ping.on('exit', function (code) {
+	    //console.log('child process exited with code ' + code);
+	    res.render('combineprocess.html', {stdout: resData});
+	    //res.write(resData, "utf8");
+	});
+
+	// child = exec('ping www.baidu.com',
+	// 	function (error, stdout, stderr) {
+	// 	    console.log('stdout: ' + stdout);
+	// 	    //res.send(stdout);
+	// 	    //console.log('stderr: ' + stderr);
+	// 	    res.render("combineprocess.html",{stdout : stdout});
+	// 	    if (error !== null) {
+	// 	      console.log('exec error: ' + error);
+	// 		}
+	// });
 };
--- routes.js
+++ routes.js
@@ -4,4 +4,5 @@ var site = require('./controllers/site');
 module.exports = function (app) {
 	// home page
 	app.get('/', site.home);
+	app.post('/combine', site.combine);
 };
